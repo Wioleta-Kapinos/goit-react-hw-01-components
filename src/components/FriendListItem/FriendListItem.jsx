2111f98b7c8caa539b8086c41c325f8569b7c737
Wioleta-Kapinos <kapinos.wioleta19@gmail.com>
2023-03-15 14:59:22 +0100

    correct style

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,8 +1,9 @@
 import css from "./FriendListItem.module.css";
 
-const FriendListItem = ({ avatar, name, isOnline }) => {
+const FriendListItem = ({ friend, avatar, name, isOnline }) => {
     return (
-        <li className={css.item}>
+        <li className={css.item}
+            key={friend}>
             <span className={isOnline ? css.statusOnline : css.statusOffline}></span>
             <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
             <p className={css.name}>{name}</p>
